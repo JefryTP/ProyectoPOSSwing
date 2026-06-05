69c94947a703398257f94713a6996d659a0fa3e5
Jefry <jefrytuncar@gmail.com>
2026-06-04 20:50:15 -0500

    inicial

---
+++ src/proyectoposswing/Main.java
@@ -0,0 +1,12 @@
+package proyectoposswing;
+
+import vistas.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VistaLogin mivista = new VistaLogin();
+        mivista.setVisible(true);
+    }
+    
+}
